e8e89d4df62841d85e2c7ccd48791bf1a0791d98
Srikanth <Srikanth@10.0.0.93>
2022-03-08 18:15:51 -0500

    First Commit.....

---
+++ src/Test1.java
@@ -0,0 +1,10 @@
+public class Test1 {
+
+	public static void main(String[] a) {
+		String i = "Java";
+		if (i == "Java")
+			System.out.println("Hello " + i);
+		else
+			System.out.println("Exit");
+	}
+}
