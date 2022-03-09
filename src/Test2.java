953ffa55b26ee8ee64ddea3e6f504dfb08b369fc
Srikanth <Srikanth@10.0.0.93>
2022-03-08 19:28:35 -0500

    Added one more check

--- src/Test2.java
+++ src/Test2.java
@@ -3,7 +3,7 @@ public class Test2 {
 
 	public static void main(String[] a) {
 		String j = "Java";
-		if (j == "Java")
+		if (j == "Java" || j.equals("Java"))
 			System.out.println("Hello " + j);
 		else
 			System.out.println("Exit");
