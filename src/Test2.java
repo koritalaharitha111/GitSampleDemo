5d869ed53269b464749af1caf639da5a86ba7f4f
Srikanth <Srikanth@10.0.0.93>
2022-03-08 19:10:33 -0500

    Commiting Test2 class to Branch

---
+++ src/Test2.java
@@ -0,0 +1,12 @@
+
+public class Test2 {
+
+	public static void main(String[] a) {
+		String j = "Java";
+		if (j == "Java")
+			System.out.println("Hello " + j);
+		else
+			System.out.println("Exit");
+	}
+
+}
